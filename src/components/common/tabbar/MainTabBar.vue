496e22ad1ad7557ab7d3a86b044839d3c64bc3d9
lghs <lguo_s@163.com>
2021-03-18 16:50:05 +0800

    添加部分文件提交

---
+++ src/components/common/tabbar/MainTabBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'tabbar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter  from 'vue-router'
+
+
+//   安装
+Vue.use(VueRouter)
+
+const router = [
+
+
+]
+
+
+//  创建路由对象
+const router = new VueRouter({
+    mode:"history",
+    routes
+})
+
+
+
+export default router
