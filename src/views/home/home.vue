756afa9779dc556ec49cd7b86c4ceaa2909917e0
lghs <lguo_s@163.com>
2021-03-23 11:52:13 +0800

    实现img功能

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,22 +2,39 @@
     <div id="home">
         <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
         <home-swiper :banners="banners"/>
-        
+        <recommend-view :recommends="recommends"/>
+        <feature-view></feature-view>
+
+        <ul>11</ul>
+        <ul>1</ul>
+        <ul>1</ul>
+        <ul>1</ul>
+        <ul>2</ul>
+        <ul>2</ul>
+        <ul>2</ul>
+        <ul>2</ul>
+        <ul>2</ul>
+        <ul>2</ul>
     </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
   import HomeSwiper from './childCopmps/HomeSwiper'
+  import RecommendView from './childCopmps/RecommendView'
+  import FeatureView from 'views/home/childCopmps/FeatureView'
 
   import {getHomeMultidata} from 'network/home'
- 
+
+    
 
 export default {
      name:"Home",  
      components: {
          NavBar,
-         HomeSwiper
+         HomeSwiper,
+         RecommendView,
+         FeatureView
      },
      data() {
          return {
@@ -39,13 +56,20 @@ export default {
 </script>
 
 <style scoped>
-    
-
+    #home {
+        padding-top: 44px;
+    }   
+ 
     .nav-bar {
     background-color: var(--color-tint);
     /* font-weight: 700; */
-    color: #fff; 
+    color: #fff;
+    
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
 
-  
 </style>
